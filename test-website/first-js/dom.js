2923655c736c50d01406b7948bc7b60457a22786
kataya1 <34242491+kataya1@users.noreply.github.com>
2020-02-26 22:56:02 +0200

    c

--- test-website/first-js/dom.js
+++ test-website/first-js/dom.js
@@ -1,1 +1,11 @@
-// to manipulte the dom tutorial
+// to manipudlte the dom tutorial
+/*const l = document.querySelectorAll('div')
+console.log(l)
+l.forEach(function item(it){console.log(it)})*/
+// document.querySelector('input.btn').remove()
+// changing the textContent of the first list element
+/*document.querySelector(".item").textContent = "hellofriend" */
+// changing the textContent of all text element and getting the index from forEach function
+/* document.querySelectorAll(".item").forEach((item, i) => item.textContent = "hello " + i )
+ */
+
