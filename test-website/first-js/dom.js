55dc8441ebd1416705656a8880f527d45199d9e5
kataya1 <34242491+kataya1@users.noreply.github.com>
2020-03-07 00:12:25 +0200

    finished the crashcourse

--- test-website/first-js/dom.js
+++ test-website/first-js/dom.js
@@ -17,11 +17,38 @@ l.forEach(function item(it){console.log(it)})*/
 
 // events
 const btn = document.querySelectorAll('.btn')
+// addEventListener() built in fucntion to take events first arguement is type  of event second argument is a funcion that gets passed the event instance
+// the function is a function to change the background color to random one using built in module Math
 btn[0].addEventListener('click', (e) => {
     e.preventDefault();
     var bg = ('#' + ('000000' +Math.floor(Math.random() * (256*256*256)).toString(16)).substr(-6))
-    var bd = document.querySelector('body')
+    const bd = document.querySelector('body')
     bd.style.backgroundColor = bg;
     bd.classList.add('aclassiwannaAddtothisElement')
     
-} )
+})
+
+const myForm = document.querySelector('#my-form')
+const nameInput = document.querySelector('#name')
+const emailInput = document.querySelector('#email')
+const msg = document.querySelector('.msg')
+const userList = document.querySelector('#users')
+
+myForm.addEventListener('submit', onSubmit)
+
+function onSubmit(e) {
+    e.preventDefault()
+    if(nameInput === '' || emailInput.value === ''){
+        msg.classList.add('error')
+        msg.innerHTML = 'enter fields bitch'
+    //  error message disapears after 3 seconds built in function setTimeout(takes functon and the time untill execution)
+        setTimeout(() => msg.remove() , 3000)
+    }
+    else {
+        console.log("bitch was successful")
+        const li = document.createElement('li')
+        // li.appendChild(document.createTextNode('${}'))
+        li.innerHTML = `${nameInput.value} :  ${emailInput.value}`
+        userList.appendChild(li)
+    }
+}
