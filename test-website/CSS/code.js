927f5be634e41951dcaeed4b2659297d95613717
kataya1 <34242491+kataya1@users.noreply.github.com>
2020-03-12 23:42:56 +0200

    m

--- test-website/CSS/code.js
+++ test-website/CSS/code.js
@@ -1,11 +1,40 @@
-const element = document.querySelectorAll('.snippet')
-
+const element = document.querySelectorAll('.snippet');
+// let r = document.styleSheets[0].cssRules
+// console.log(r)
 // const style = getComputedStyle(element)
-
+// function css_text(x) { return x.cssText; }
+// implenment it using foreach or map later
+function plainText(x) {
+    arr = []
+    for(let char of x){
+        c = char
+        if(char === "<"){
+           c = "&lt;"
+        }
+        else if (char === '>'){
+            c = "&gt;"
+        }
+        arr.push(c)
+    }
+    return arr.join('')
+}
 const foo = (element) => {
     for(v of element){
         console.log(v)
+        let visual = v.querySelector('.visual')
+        let code = v.querySelector('.code')
+        let inHtml = visual.innerHTML
+        console.log(inHtml)
+        // console.log(plainText(inHtml))
+        // let arr = []
+        // for(child of visual.children){
+        //     console.log(child)
+        //     // var content = Array.prototype.map.call(child.cssStylesheet.cssRules, css_text).join('\n');
+
+        // }
+        console.log(`HTML<pre><code>${plainText(inHtml)}</code></pre><hr>CSS<br> <code>${code.innerHTML}</code>`)
+        code.innerHTML=`HTML<pre><code>${plainText(inHtml)}</code></pre><hr>CSS<br> <code>${code.innerHTML}</code>`
+        // console.log(arr)
     }
 }
-
 foo(element)
