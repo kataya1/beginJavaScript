2a4fc29e93f80a6e1de0de4190440ef60a4a6a99
kataya1 <34242491+kataya1@users.noreply.github.com>
2020-03-18 21:45:50 +0200

    iframe update

--- test-website/CSS/code.js
+++ test-website/CSS/code.js
@@ -1,3 +1,4 @@
+// get's the snippet from the html code you want to fill in
 const element = document.querySelectorAll('.snippet');
 // let r = document.styleSheets[0].cssRules
 // console.log(r)
@@ -5,6 +6,7 @@ const element = document.querySelectorAll('.snippet');
 // function css_text(x) { return x.cssText; }
 // implenment it using foreach or map later
 function plainText(x) {
+    // changes html tags to non interpretable text by the html interpreter
     arr = []
     for(let char of x){
         c = char
@@ -18,7 +20,9 @@ function plainText(x) {
     }
     return arr.join('')
 }
+// v1 retired
 const foo = (element) => {
+    // this takes the content visual section and prints it in the code section
     for(v of element){
         console.log(v)
         let visual = v.querySelector('.visual')
@@ -37,4 +41,21 @@ const foo = (element) => {
         // console.log(arr)
     }
 }
+
 foo(element)
+const foo2 = (element) => {
+    // this takes the content visual section and prints it in the code section
+    for(v of element){
+        console.log(v)
+        let visual = v.querySelector('.visual')
+        let code = v.querySelector('.code')
+        let ifrm = visual.querySelector("iframe")
+        
+        let inHtml = visual.innerHTML
+
+
+        console.log(`HTML<pre><code>${plainText(inHtml)}</code></pre><hr>CSS<br> <code>${code.innerHTML}</code>`)
+        code.innerHTML=`HTML<pre><code>${plainText(inHtml)}</code></pre><hr>CSS<br> <code>${code.innerHTML}</code>`
+        // console.log(arr)
+    }
+}
